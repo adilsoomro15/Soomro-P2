08de687805b8ad81583782ab2c82da96a486d513
Adil Soomro <adilgulsoomro@gmail.com>
2024-01-30 11:37:06 -0500

    Fixed Milestone #1

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function () {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (/* function */ callback, /* DOMElement */ element) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,7 +18,7 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
+	requestAnimFrame(animate);
 	var currentTime = new Date().getTime();
 	if (mLastFrameTime === 0) {
 		mLastFrameTime = currentTime;
@@ -54,27 +54,28 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = images.json;
+var mUrl = "images.json";
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
+	return function (e) {
 		galleryImage.img = e.target;
 		mImages.push(galleryImage);
 	}
 }
 
-$(document).ready( function() {
-	
+$(document).ready(function () {
+
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
+	fetchJSON()
+
 });
 
-window.addEventListener('load', function() {
-	
+window.addEventListener('load', function () {
+
 	console.log('window loaded');
 
 }, false);
@@ -82,23 +83,27 @@ window.addEventListener('load', function() {
 function GalleryImage() {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
-	let location 
+	let location
 	//2. description of photo
-	let description 
+	let description
 	//3. the date when the photo was taken
-	let date 
+	let date
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-	let img 
+	let img
 }
 
-function fetchJSON(images.JSON) {
-	mRequest.open('GET', 'mJson')
-	mRequest.onreadystatechange function () {
-		if (mRequest.readyState >= 200 && mJson.status < 400) {
-			var data = JSON.parse(mRequest.responseText);
+function fetchJSON() {
+	// When the XMLHttpRequest() state changes, it will trigger a function.
+	mRequest.onreadystatechange = function () {
+		// If it loads, it will parse the JSON file. and get the text from it.
+		if (mRequest.readyState == 4 && mRequest.status == 200) {
+			var mJson = JSON.parse(mRequest.responseText);
 		} else {
+			// If it doesn't load, it will send an error message in the console.
 			console.log("We connected to the server, but it returned an error.");
 		}
-		mRequest.send()
 	}
+	// The XMLHttpRequest() will open and send.
+	mRequest.open("GET", mUrl, true)
+	mRequest.send()
 }
